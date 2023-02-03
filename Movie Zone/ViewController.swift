baae1a12a1af273bea142550238d223e2a06f94e
Monish Chaudhari <monishchaudhari0@gmail.com>
2023-02-03 18:30:34 +0530

    Initial Comit

---
+++ Movie Zone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie Zone
+//
+//  Created by Monish Chaudhari on 03/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
